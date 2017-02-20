0a1485c0d729e2ee17da5f3179a13ef621f95990
Simon Oulevay <simon.oulevay@gmail.com>
2017-02-20 18:24:42 +0100

    POST /actors

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
+const actorsApi = require('./routes/api/actors');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,25 +18,35 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api/actors', actorsApi);
 app.use('/', index);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// API error handler (responds with JSON)
+app.use('/api', function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
+
+// Generic error handler (responds with HTML)
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
