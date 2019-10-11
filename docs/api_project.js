8bd45e44ec6eec1e7ee1ec8fb9755371e7f8d802
Arthur Verdon <verdon.arthur@gmail.com>
2019-10-11 15:31:16 +0200

    change app version

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "Express REST Demo",
-  "version": "1.0.0",
+  "version": "1.1.0",
   "description": "A demonstration REST API developed with Express",
   "title": "Express REST Demo",
   "url": "https://evening-meadow-25867.herokuapp.com",
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-10-11T13:29:54.236Z",
+    "time": "2019-10-11T13:31:04.408Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
