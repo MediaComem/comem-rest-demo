0cd00f050de38a321d56046b5ea5e7ade2964193
Simon Oulevay <simon.oulevay@gmail.com>
2020-09-11 10:31:45 +0200

    Fix and improve automated tests

--- spec/people.spec.js
+++ spec/people.spec.js
@@ -4,6 +4,7 @@ const supertest = require('supertest');
 
 const app = require('../app');
 const { baseUrl } = require('../config');
+const Movie = require('../models/movie');
 const Person = require('../models/person');
 const { cleanUpDatabase } = require('./utils');
 
@@ -19,11 +20,11 @@ describe('POST /api/people', function() {
       .send({
         name: 'John Doe',
         gender: 'male'
-      })
-      .set('Content-Type', 'application/json')
-      // Check that the response is 201 Created with a JSON body.
-      .expect(201)
-      .expect('Content-Type', /json/);
+      });
+
+    // Check that the status and headers of the response are correct.
+    expect(res.status, 'res.status').to.equal(201);
+    expect(res.get('Content-Type'), 'res.headers.Content-Type').to.have.string('application/json');
 
     // Check that the response body is the created person.
     const body = res.body;
@@ -34,29 +35,36 @@ describe('POST /api/people', function() {
     expect(body.name, 'res.body.name').to.equal('John Doe');
     expect(body, 'res.body').to.have.all.keys('createdAt', 'gender', 'id', 'name');
 
-    // Check the location header.
-    expect(res.get('Location'), 'res.headers.location').to.equal(`${baseUrl}/api/people/${body.id}`);
+    // Check that the Location header points to the correct resource.
+    expect(res.get('Location'), 'res.headers.Location').to.equal(`${baseUrl}/api/people/${body.id}`);
   });
 });
 
 describe('GET /api/people', function() {
 
-  // Create 2 people in the database before each test in this block.
   beforeEach(async function() {
-    await Promise.all([
+    // Create 2 people in the database before each test in this block.
+    const [ johnSmith, janeSmith ] = await Promise.all([
       Person.create({ name: 'John Smith', gender: 'male' }),
       Person.create({ name: 'Jane Smith', gender: 'female' })
     ]);
+
+    // Also create 3 movies directed by the 2 people.
+    await Promise.all([
+      Movie.create({ title: 'An Amazing Story', rating: 10, directorId: janeSmith.id }),
+      Movie.create({ title: 'A Bad Story', rating: 2, directorId: janeSmith.id }),
+      Movie.create({ title: 'A So-So Story', rating: 4, directorId: johnSmith.id })
+    ])
   });
 
   it('should retrieve a list of people', async function() {
 
     // Make a GET request on /api/people.
-    const res = await supertest(app)
-      .get('/api/people')
-      // Check that the response is 200 OK with a JSON body.
-      .expect(200)
-      .expect('Content-Type', /json/);
+    const res = await supertest(app).get('/api/people');
+
+    // Check that the status and headers of the response are correct.
+    expect(res.status, 'res.status').to.equal(200);
+    expect(res.get('Content-Type'), 'res.headers.Content-Type').to.have.string('application/json');
 
     // Check that the response body is an array.
     const body = res.body;
@@ -65,7 +73,7 @@ describe('GET /api/people', function() {
     // Check that the first person is the correct one.
     expect(body[0].birthDate, 'res.body[0].birthDate').to.equal(null);
     expect(body[0].createdAt, 'res.body[0].createdAt').to.be.a('string');
-    expect(body[0].directedMovies, 'res.body[0].directedMovies').to.equal(1);
+    expect(body[0].directedMovies, 'res.body[0].directedMovies').to.equal(2);
     expect(body[0].gender, 'res.body[0].gender').to.equal('female')
     expect(body[0].id, 'res.body[0].id').to.be.a('string');
     expect(body[0].name, 'res.body[0].name').to.equal('Jane Smith');
